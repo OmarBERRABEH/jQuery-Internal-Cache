7e6ddaf41801fde1599a5cea6a9176843e411f74
Hajji Tarik <hajji-tarik@outlook.fr>
2017-03-21 20:03:50 +0000

    Update jquery.internalcache.js

--- jquery.internalcache.js
+++ jquery.internalcache.js
@@ -1,27 +1,25 @@
 'use strict';
 
-const GET_NODE_CACHE = (function($) {
-    // save the noop function to  return if the jQuery no exist
-    let fn = function() {};
-    try {
-        //check the presence of the jQuery instance
-        if ($ === undefined) {
-            throw 'jQuery libs not exist';
-        }
+const GET_NODE_CACHE = ($ => {
+  // save the noop function to  return if the jQuery no exist
+  let fn = () => {};
+  try {
+    //check the presence of the jQuery instance
+    if ($ === undefined) throw new Error('jQuery libs not exist');
 
-        // the UID jquery instance
-        const $UID = $.expando;
+    // the UID jquery instance
+    const $UID = $.expando;
 
-        /**
-        * @param node the DOM to be interogate 
-        **/
-        fn = function getNodeCache(node) {
-        	let idNode = node[$UID];
-        	return idNode && $.cache[idNode];
-        }
-
-    } catch (e) {
-        console.warn(e);
+    /**
+     * @param node the DOM to be interogate
+     **/
+    fn = (node) => {
+      const idNode = node[$UID];
+      return idNode && $.cache[idNode];
     }
-    return fn;
+
+  } catch (e) {
+    console.warn(e);
+  }
+  return fn;
 })(window.jQuery);
